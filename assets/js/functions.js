64d396c5fd6d352a689a0dc30eb33ae71996025b
Jacquin Wynn <jacquinwynn@Jacquins-MacBook-Pro.local>
2016-03-26 20:38:37 -0400

    Adding pages

---
+++ assets/js/functions.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	//Start typing here!
+
+	var whatIsLife = true,
+		option1 = "Software Is Life 🖥 💻 🖥",
+		option2 = "Ball is Life 🏀 🏀 🏀",
+		option3 = "Idk what Life is! 😟 ";
+
+	if (whatIsLife) {
+	  console.log("What is life? " + option1 );
+	}
+
+});
